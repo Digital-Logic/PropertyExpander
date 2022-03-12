0c3ceada541026820b961831213b2a760d2b7e26
lbrown <lbrown@digital-logic.net>
2022-03-12 13:57:26 -0700

    Initial commit

---
+++ src/test/java/net/digitallogic/propertyexpander/PropertyExpanderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.digitallogic.propertyexpander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyExpanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
